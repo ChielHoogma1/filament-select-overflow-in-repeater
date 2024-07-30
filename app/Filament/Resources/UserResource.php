810049dd2cc0f126362f8c27fda34d9d76a91253
chiel <chiel.hoogma@scoreagency.nl>
2024-07-30 13:14:23 +0200

    Initial commit

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Notifications\Notification;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Repeater::make('repeater')
+                    ->collapsed()
+                    ->live()
+                    ->schema([
+                        Forms\Components\Select::make('Non hidden select')
+                            ->translateLabel()
+                            ->searchable()
+                            ->options(['test'=>'test', 'test123'=>'test123']),
+                        Checkbox::make('show/hide field')
+                            ->translateLabel()
+                            ->live(),
+                        Select::make('hidden select')
+                            ->translateLabel()
+                            ->searchable()
+                            ->required()
+                            ->options(['test'=>'test', 'test123'=>'test123'])
+                            ->visible(fn (Get $get) => $get('show/hide field')),
+                    ])->reorderable()->orderColumn('order')
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
